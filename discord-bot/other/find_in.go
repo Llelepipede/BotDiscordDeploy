0af167b20b105d3646388a9cc86940d54a02bee0
Llelepipede <pchesneau3103@gmail.com>
2022-07-06 17:55:21 +0200

    after the crash + split

--- discord-bot/other/find_in.go
+++ discord-bot/other/find_in.go
@@ -26,3 +26,42 @@ func Find_in_api(to_find string, in []data.Api) (int, bool) {
 	}
 	return ret, false
 }
+
+func Split(str string) []string {
+	runes := []rune(str)
+	count := 0
+	prevIsLeter := false
+	for _, val := range runes {
+		if val == '\n' || val == '\t' || val == ' ' {
+			if prevIsLeter {
+				count++
+				prevIsLeter = false
+			}
+		} else {
+			prevIsLeter = true
+		}
+	}
+	if prevIsLeter {
+		count++
+	}
+	resArr := make([]string, count)
+	i := 0
+	start := 0
+	prevIsLeter = false
+	for ind, val := range runes {
+		if val == '\n' || val == '\t' || val == ' ' {
+			if prevIsLeter {
+				resArr[i] = string(runes[start:ind])
+				i++
+				prevIsLeter = false
+			}
+			start = ind + 1
+		} else {
+			prevIsLeter = true
+		}
+	}
+	if prevIsLeter {
+		resArr[i] = string(runes[start:])
+	}
+	return resArr
+}
